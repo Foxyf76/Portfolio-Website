00679fe8940cb245469da5d15b9f708b2e442acc
Luke Fox <luke.fox@distilled.ie>
2019-01-30 16:24:33 +0000

    Added hyperlinks at index, added some more blog entries

--- server.js
+++ server.js
@@ -1,48 +1,46 @@
-const express = require('express');
-const exphbs  = require('express-handlebars');
+const express = require("express");
+const exphbs = require("express-handlebars");
 const app = express();
-///
-
-const fs = require('fs');
-
 
 ///
 
+var fs = require("fs");
 
 // Register Handlebars view engine
-app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 
 // Use Semantic UI
-app.use("/semantic", express.static(__dirname + '/semantic'));
+app.use("/semantic", express.static(__dirname + "/semantic"));
 
-app.use(express.static(__dirname + '/views'));
-app.use("/css", express.static(__dirname + '/css'));
-app.use("/assets", express.static(__dirname + '/assets'));
-app.use("/counter.json", express.static(__dirname + '/counter.json'));
+app.use(express.static(__dirname + "/views"));
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/assets", express.static(__dirname + "/assets"));
+app.use("/counter.json", express.static(__dirname + "/counter.json"));
+app.use("/server.js", express.static(__dirname + "/server.js"));
 
 // Use Handlebars view engine
-app.set('view engine', '.hbs');
+app.set("view engine", ".hbs");
 
-app.get('/', (req, res) => {
-  res.render('index.hbs');
+app.get("/", (req, res) => {
+  res.render("index.hbs");
 });
 
-app.get('/about', (req, res) => {
-    res.render('about.hbs');
-  });
+app.get("/about", (req, res) => {
+  res.render("about.hbs");
+});
 
-app.get('/more', (req, res) => {
-    res.render('more.hbs');
-  });
+app.get("/more", (req, res) => {
+  res.render("more.hbs");
+});
 
-app.get('/blog', (req, res) => {
-    res.render('blog.hbs');
-  });
+app.get("/blog", (req, res) => {
+  res.render("blog.hbs");
+});
 
-  app.get('/blog/january', (req, res) => {
-    res.render('january.hbs');
-  });
+app.get("/blog/january", (req, res) => {
+  res.render("january.hbs");
+});
 
 app.listen(3000, () => {
-  console.log('App is running → PORT 3000');
+  console.log("App is running → PORT 3000");
 });
